0df1addde713a5dd430cc39ebb8fff1238a2443f
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-07 08:08:29 -0500

    Mutation is working but return null

--- api/src/graphql/schema.js
+++ api/src/graphql/schema.js
@@ -1,16 +1,29 @@
 const {buildSchema} = require('graphql');
 const {getAllProducts} = require("../services/productsService")
+const {modifyProduct} = require("../services/productsService")
 
 const root = {
     product:()=>{
         console.log("----------------------------weufhwiufhiuwefhiw")
         return getAllProducts()
+    },
+    modifyProduct:(id,dataToModify)=>{
+        return modifyProduct({id,dataToModify})
     }
 }
 
 const schema = buildSchema(`#La Query raiz
 type Query{
     product: [product]
+}
+type Mutation{
+    modifyProduct(id: Int): product
+}
+input inputProduct{
+    description: String,
+    price: String,
+    stock: Int,
+    image: String,
 }
 type product{
     id : Int
