f469a030c1b563d12e993c30bfad8e90a245106f
Lucas Uriel Acosta <lucasuracosta@gmail.com>
2021-04-08 11:42:12 -0300

    Set all routes

--- client/src/Components/screens/catalogue/container/Catalogue.jsx
+++ client/src/Components/screens/catalogue/container/Catalogue.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import NavBar from '../../navBar/NavBar'
-import Hero from '../hero/Hero'
-import Products from '../products/container/Products'
+import React from 'react';
+import Hero from '../hero/Hero';
+import Products from '../products/container/Products';
 
 const Catalogue = () => {
     return (
         <>
-            <NavBar />
             <Hero />
             <Products />
         </>
