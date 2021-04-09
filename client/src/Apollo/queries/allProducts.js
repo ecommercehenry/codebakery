2653bf311ae86d482278908ba1be4c27b3c27076
johana1211 <johanarezabala1211@gmail.com>
2021-04-09 14:42:12 -0300

    S6_grid

--- client/src/Apollo/queries/allProducts.js
+++ client/src/Apollo/queries/allProducts.js
@@ -7,6 +7,7 @@ query{
     description
     id
     image
+    price
   }
 }
 
--- client/src/Components/screens/catalogue/products/grid/Grid.jsx
+++ client/src/Components/screens/catalogue/products/grid/Grid.jsx
@@ -4,63 +4,7 @@ import allProducts from "../../../../../Apollo/queries/allProducts.js";
 import { useQuery } from '@apollo/client';
 import "./grid.css";
 
-const product = [
-  {
-    id: 1,
-    title: "Brawnie",
-    precio: "25$",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmy8dBUK-2SrQy0ExK-6OjyoxRHEYAKTeruQ&usqp=CAU",
-  },
-  {
-    id: 2,
-    title: "Galletas",
-    precio: "40$",
-    image:
-      "https://www.hacergalletas.com/wp-content/uploads/2019/07/receta-galletas-caseras-de-mantequilla-1000.jpg",
-  },
-  {
-    id: 3,
-    title: "Biscocho",
-    precio: "45$",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmy8dBUK-2SrQy0ExK-6OjyoxRHEYAKTeruQ&usqp=CAU",
 
-  },
-  {
-    id: 4,
-    title: "Amapola",
-    precio: "15$",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmy8dBUK-2SrQy0ExK-6OjyoxRHEYAKTeruQ&usqp=CAU",
-  },
-  {
-    id: 5,
-    title: "Cookie",
-    precio: "10$",
-    image:
-      "https://lh3.googleusercontent.com/proxy/7zdjPHwcWadhJPpqyxBvYg6icJ2At1TD8Is5yatLnRBT_qbfap9zsQIN1kpki6lUjW0HXbdZLqBZiRzEImedc2XgHRveZ_NLiLawoll-LtG5j5tzcc897HRg58iBwA9yWVZwpYf_QQx-xa8qlijmyHj4gGz76qOR6WftGYPPdJy2wDZySYvIp-l-FIlPtFGBMFn7Fv3NRzZpAbzjA4Pv773Tx3G5R0gj085_-GU6m8urm64a6o8",
-  },
-  {
-    id: 6,
-    title: "Espiral",
-    precio: "20$",
-    image:
-      "https://www.hola.com/imagenes/cocina/tecnicas-de-cocina/20201110178912/brownie-de-chocolate-receta/0-887-553/brownie-t.jpg",
-  },
-  {
-    id: 7,
-    title: "Galletas",
-    precio: "40$",
-    image:
-      "https://www.hacergalletas.com/wp-content/uploads/2019/07/receta-galletas-caseras-de-mantequilla-1000.jpg",
-  },
-  {
-    id: 8,
-    title: "Galletas",
-    precio: "40$",
-    image:
-    'https://www.hola.com/imagenes/cocina/tecnicas-de-cocina/20201110178912/brownie-de-chocolate-receta/0-887-553/brownie-t.jpg',
-  },
-];
 // {
 //     query getAllProducts{
 //         product{
@@ -72,30 +16,26 @@ const product = [
 // }
 // Debo correjir por que trae los datos pero productos sale undefined; 
 const Grid = ({id}) => {
-
       const { data } = useQuery(allProducts)
       console.log(data)
       useEffect(() => {
-        
     }, [data])
 
   return (
 
      <div className="container">
       {
-      product?.map((element) => (
+      data?.product.map((element) => (
         <div>
           <span className="card" key={element.id}>
-          <Link to={`/product/${id}`}>
             <img
               src={element.image}
               width="250"
               height="200"
               alt="No se encontro la imagen"
             />
-            </Link>
-            <h4 className='title'>{element.title}</h4>
-            <p className='price'>{element.precio}</p>
+            <h4 className='title'>{element.name}</h4>
+            <p className='price'>{element.price}</p>
           </span>
         </div>
       ))
