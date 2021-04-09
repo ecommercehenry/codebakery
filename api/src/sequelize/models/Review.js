dbd7d684c79294b942065fdf29a142951efb498b
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-04-09 08:35:29 -0500

    Crea_Modelo_Review

---
+++ api/src/sequelize/models/Review.js
@@ -0,0 +1,25 @@
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+const { DataTypes } = require("sequelize")
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("review", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stars: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  })
+}
