1f22f4f58250bdd59590ce9fd4733335b62bf462
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-04-09 08:15:12 -0500

    Crea_Modelo_User

---
+++ api/src/sequelize/models/User.js
@@ -0,0 +1,47 @@
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+const bcrypt = require("bcrypt-nodejs")
+const { DataTypes } = require("sequelize")
+
+module.exports = (sequelize, DataTypes) => {
+  // defino el modelo
+  const User = sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    // Esto es para hashear la contraseña
+    {
+      freezeTableName: true,
+      instanceMethods: {
+        generateHash(password) {
+          return bcrypt.hash(password, bcrypt.genSaltSync(8))
+        },
+        validPassword(password) {
+          return bcrypt.compare(password, this.password)
+        },
+      },
+    }
+  )
+  return User
+}
