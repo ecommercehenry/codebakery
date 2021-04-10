12d9be54871ca16fc91d2a7e8624e7c565090134
Lucas Uriel Acosta <lucasuracosta@gmail.com>
2021-04-10 00:13:18 -0300

    Fix conflicts 2

--- client/src/Components/GlobalStyle.jsx
+++ client/src/Components/GlobalStyle.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import styled, {createGlobalStyle, css } from 'styled-components';
-import {Link} from 'react-router-dom'
-=======
 import { createGlobalStyle } from 'styled-components';
->>>>>>> 40e401303ef15b56c1111ab15d6e69673489c6f7
 
 const GlobalStyle = createGlobalStyle`
     *{
