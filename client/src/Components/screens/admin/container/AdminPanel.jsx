740b8c2fbd180b16a8905ed3ee156e660a8c8df6
ggonzalescbs <guillermogonzalescbs@gmail.com>
2021-04-09 23:22:44 -0300

    add changes to admin panel

--- client/src/Components/screens/admin/container/AdminPanel.jsx
+++ client/src/Components/screens/admin/container/AdminPanel.jsx
@@ -6,14 +6,26 @@ import styled from 'styled-components';
 //components
 import LeftPanel from '../LeftPanel';
 import AdminNavBar from '../AdminNavBar';
+import AddProductForm from '../../../AddProductForm';
 import FormCRUD from '../../../FormCRUD'
 
 const AdminPanel = () => {
     return (
         <StyledAdminPanel>
-            <LeftPanel/>
-            <AdminNavBar/>
-            {/* <FormCRUD /> */}
+            <div className="left">
+                <LeftPanel/>
+            </div>
+            <div className="right">
+                <div className="top">
+                <AdminNavBar/>
+                </div>
+                <div className="bottom">
+                    <AddProductForm/>
+                </div>
+                
+                {/* <FormCRUD /> */}
+            </div>
+            
         </StyledAdminPanel>
     )
 }
@@ -22,7 +34,28 @@ const StyledAdminPanel = styled.div`
     min-height: 100vh;
     width: 100%;
     display:flex;
-
+    flex-direction:row;
+    justify-content:space-between;
+    background: black;
+    .left{
+        width:13%;
+        //background:green;
+    }
+    .right{
+        width:87%;
+        //background:yellow;
+        display:flex;
+        flex-direction:column;
+        .top{
+            height:15vh;
+        }
+        .bottom{
+            height:85vh;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+        }
+    }
 `;
 
 export default AdminPanel
