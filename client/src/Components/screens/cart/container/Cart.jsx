c726f89064b4caa1af64c4b4def328db53b9f4ab
Nicolas Lohuandus <nlohuandus@gmail.com>
2021-04-12 20:39:18 -0300

    Update Cart.jsx

--- client/src/Components/screens/cart/container/Cart.jsx
+++ client/src/Components/screens/cart/container/Cart.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import productById from '../../../../Apollo/queries/productById'
+import {useQuery} from '@apollo/client'
 
-const Cart = () => {
-    return (
-        <div>
-            You are in "/cart" route
-        </div>
-    )
-}
+const Cart = (dataDePrueba = [1,2,5,3,8]) => {
+    let storage = window.localStorage;
+    // localStorage.setItem('cart',  );
+    // var cart = localStorage.getItem('cart');
+    let [items, setItems] = useState([]);
 
-export default Cart
+    console.log(items, storage)
+  return <div> carrito </div>;
+};
+
+export default Cart;
