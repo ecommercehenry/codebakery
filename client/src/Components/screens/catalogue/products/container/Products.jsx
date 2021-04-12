cb5b498e1fc5056eb58b75d8a2903ffc66a54271
ggonzalescbs <guillermogonzalescbs@gmail.com>
2021-04-11 23:22:56 -0300

    remove empty css from products component

--- client/src/Components/screens/catalogue/products/container/Products.jsx
+++ client/src/Components/screens/catalogue/products/container/Products.jsx
@@ -4,7 +4,7 @@ import Categories from '../categories/Categories'
 import HomeButton from '../home&sort/homeButton/HomeButton'
 import SortButton from '../home&sort/sortButton/SortButton'
 import Grid from '../grid/Grid'
-import "./Products.css"; 
+//import "./Products.css"; 
 
 const Products = () => {
     console.log('lucas')
