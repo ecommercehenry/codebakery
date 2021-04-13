92f2e06d6e6a5b192ba74cc4d8237d4263283573
franciscotov <frank.ronaldo_17@hotmail.com>
2021-04-12 21:43:54 -0300

    modificacion de usuario v2.1

--- api/src/graphql/roots/mutationsResolver/modifyUser.js
+++ api/src/graphql/roots/mutationsResolver/modifyUser.js
@@ -2,9 +2,7 @@ const { modifyUser } = require("../../../services/userService");
 
 module.exports = {
     modifyUser: async ({id,name, password, email, role}) => {
-        // let {name, password, email, role} = args.input;
         let num = await modifyUser(id , name, password, email, role);
-        console.log(num)
-        return num[0];
+        return num.dataValues;
     }
 }
--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -20,11 +20,8 @@ async function modifyUser(id, name, password, email, role) {
   if(password) obj.password = password;
   if(email) obj.email = email;
   if(role) obj.role = role;
-  return await Users.update({ ...obj }, { where: { id } });
-  // si se pide retorno de usuario modificado
-  let user = await Users.update({ ...obj }, { where: { id } });
-  if(user[0] == 1) return await Users.findOne({ where: { id } });
-  else return {id, name: 'Error', password, email, role};
+  let user = await Users.findOne({ where: { id } });
+  return await user.update(obj);
 }
 
 module.exports = { getAllUsers, createUser, modifyUser}
