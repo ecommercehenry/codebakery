8d44009d5c95203af57133f8a21ee79cb90cf9c8
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-14 11:16:52 -0500

    added users

--- api/src/sequelize/dataPopulation.js
+++ api/src/sequelize/dataPopulation.js
@@ -1,11 +1,17 @@
-const { Product, Category, conn } = require("../db");
+const { Product, Category, Users, conn } = require("../db");
+const { createUser } = require("../services/userService");
 
 const MOCK_CATEOGRIES = require("./MOCK_CATEOGRIES.json");
 const MOCK_PRODUCTS = require("./MOCK_PRODUCTS.json");
+const MOCK_USER = require("./MOCK_USER.json");
 
 async function dataPopulation() {
     await Category.bulkCreate(MOCK_CATEOGRIES),
     await Product.bulkCreate(MOCK_PRODUCTS)
+    await createUser("admin","12345","admin@admin.com","admin")
+    await createUser("juan","12345","juan@gmail.com","user")
+
+    // await Users.bulkCreate(MOCK_USER)
     await conn.query(`insert into "product-category" ("productId","categoryId") values (1,1)`)
     await conn.query(`insert into "product-category" ("productId","categoryId") values (1,2)`)
     await conn.query(`insert into "product-category" ("productId","categoryId") values (1,3)`)
