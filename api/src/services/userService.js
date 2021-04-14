f7d865ffee9f79b0ee454d2adc189ab608c5d8ef
Paulus <elxebec@gmail.com>
2021-04-14 18:01:30 -0300

    Arreglado bug/ duplicity key

--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -1,24 +1,29 @@
-const { Users } = require("../db")
+const { Users } = require("../db");
 
 async function getAllUsers() {
   try {
-    return await Users.findAll()
+    return await Users.findAll();
   } catch (error) {
     throw new Error(error);
   }
 }
 
 async function createUser(name, password, email, role) {
-  try {  
-    return await Users.create({
-      name,
-      password,
-      email,
-      role
-    })
-  } catch (error) {
-    throw new Error(error);
+  const validationUser = await Users.findAll({
+    where: { email: email },
+  });
+  if (validationUser.length === 0) {
+    try {
+      return await Users.create({
+        name,
+        password,
+        email,
+        role,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
 
-module.exports = { getAllUsers, createUser }
+module.exports = { getAllUsers, createUser };
