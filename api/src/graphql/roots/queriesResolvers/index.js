e3f325215e69977b0d7a05030f712987b9a271da
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-04-14 07:02:55 -0500

    Fix conflicts

--- api/src/graphql/roots/queriesResolvers/index.js
+++ api/src/graphql/roots/queriesResolvers/index.js
@@ -1,13 +1,18 @@
-const {categories} = require("./categories");
-const {product} = require("./product");
-const {productById} = require("./productById");
-const {getProductByCategoryName} = require("./productByCategoryName");
-const {getAllCategories}= require("./categories");
-const {productCategory} = require("./productCategory");
-const {getProductByName } = require("./getProductByProductName")
-const {getProductByArray} = require("./getProductByArray")
-const {validateUser} = require("./validateUser")
-
+const { categories } = require("./categories")
+const { product } = require("./product")
+const { productById } = require("./productById")
+const { getProductByCategoryName } = require("./productByCategoryName")
+const { getAllCategories } = require("./categories")
+const { productCategory } = require("./productCategory")
+const { getAllUsers } = require("./getAllUsers")
 
 // getProductByCategoryName
-module.exports = {categories, product, productById , getProductByCategoryName, getAllCategories,productCategory, getProductByName, getProductByArray,validateUser}
+module.exports = {
+  categories,
+  product,
+  productById,
+  getProductByCategoryName,
+  getAllCategories,
+  productCategory,
+  getAllUsers,
+}
