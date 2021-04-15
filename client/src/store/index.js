b982e3f4aff656d96ee5b644f5970ff1607a24c7
ggonzalescbs <guillermogonzalescbs@gmail.com>
2021-04-15 18:38:47 -0300

    add changes to store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,11 @@
 import {applyMiddleware, combineReducers, createStore} from "redux"
+import { persistStore } from 'redux-persist';
 import {composeWithDevTools} from "redux-devtools-extension"
 import thunk from "redux-thunk"
-import productsReducer from "../reducer/productsReducer";
-import reducer from "../reducer/reducer"
-const rootReducer = combineReducers({
-  productsReducer,
-  reducer
-})
-const store = createStore(rootReducer,composeWithDevTools(
-    applyMiddleware(thunk)
-))
+import rootReducer from "./rootReducer"
 
-export default store;
+export const store = createStore(rootReducer,composeWithDevTools(
+  applyMiddleware(thunk)
+))
+export const persistor = persistStore(store);
+export default {store,persistor};
