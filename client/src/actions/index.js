b1510f1524546bb87a700fa7c495526a2ca46231
ggonzalescbs <guillermogonzalescbs@gmail.com>
2021-04-15 19:13:29 -0300

    add changes to actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,7 @@ export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
 export const GET_PRODUCT_BY_NAME = "GET_PRODUCT_BY_NAME";
 export const GUARDAR_PRODUCTOS = "GUARDAR_PRODUCTOS";
 export const SET_SEARCH = 'SET_SEARCH'; 
-export const ADD_PRODUCT_TO_CART = "ADD_PRODUCT_TO_CART";
-export const REMOVE_PRODUCT_TO_CART = "REMOVE_PRODUCT_TO_CART";
+
 
 export const setSearch = () => {
   return {
@@ -30,19 +29,5 @@ export const fetchByName = (product) => {
   return {
     type: GET_PRODUCT_BY_NAME,
     payload: product,
-  };
-};
-
-export const addProductToCart = (args) => {
-  return {
-    type: ADD_PRODUCT_TO_CART,
-    payload: args
-  };
-};
-
-export const removeProductFromCart = (id) => {
-  return {
-    type: REMOVE_PRODUCT_TO_CART,
-    payload: id
   };
 };
