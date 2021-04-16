2fccba934b9bff5687c9491749f5aad93eb4b0ae
Nicolas Lohuandus <nlohuandus@gmail.com>
2021-04-16 14:45:32 -0300

    rendering?

--- client/src/Components/screens/cart/UserCart.jsx
+++ client/src/Components/screens/cart/UserCart.jsx
@@ -13,23 +13,22 @@ const UserCart = () => {
   const {data, loading} = useQuery(GET_ORDERS_BY_USER_ID_IN_CART, {variables:{idUser: userId} });
 
 console.log(data)
-  return <p>si ves esto no rompi</p>
-//   <StyledCart>
-//   {data ? (
-//     data.getOrdersByUserIdInCart[0].lineal_order.map((elem) => (
-//       <ProductOnCart
-//         id={elem.id}
-//         name={elem.lineal_order[0].product[0].name}
-//         price={elem.lineal_order[0].product[0].price}
-//         stock={elem.lineal_order[0].product[0].stock}
-//         image={elem.lineal_order[0].product[0].image}
-//         quantity={elem.lineal_order[0].quantity}
-//       />
-//     ))
-//   ) : (
-//     <p>vacio</p>
-//   )}
-// </StyledCart>
+  return   <StyledCart>
+  {data?.getOrdersByUserIdInCart? (
+    data.getOrdersByUserIdInCart.orders.map((order) => (
+        <ProductOnCart
+          id={5}
+          name={'queso crema'}
+          price={3}
+          stock={15}
+          image={'lol'}
+          quantity={10}
+        />
+    ))
+  ) : (
+    <p>vacio</p>
+  )}
+</StyledCart>
 };
 const StyledCart = styled.div`
   //background: black;
