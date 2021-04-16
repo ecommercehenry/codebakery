21e557ab62fd24412e22c77dc570aefbe6e2e828
Wen <lizen777@gmail.com>
2021-04-16 16:20:14 -0300

    para hacer rutas

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import UserAccount from "./Components/UserAcount/UserAccount";
 import CreateUserAccount from "./Components/UserAcount/CreateUserAccount";
 import TablaOrdenes from "./Components/screens/admin/ordenes/TablaOrdenes"
 
+
 function App() {
   return (
     <>
