2d61a44584b1765e334ce67cd05762032e8c8b92
ggonzalescbs <guillermogonzalescbs@gmail.com>
2021-04-17 11:56:43 -0300

    add changes to button

--- client/src/Components/screens/catalogue/products/grid/ButtonAddCart.jsx
+++ client/src/Components/screens/catalogue/products/grid/ButtonAddCart.jsx
@@ -20,18 +20,32 @@ const ButtonAddCart = ({id}) => {
     )
 }
 
-const StyledButton = styled.button`
+const media = {
+    tablet: '@media(min-width:768px)',
+    laptop: '@media(min-width:992px)',
+    desktop: '@media(min-width:1200px)',
+}
 
+const StyledButton = styled.button`
     display:flex;
     flex-direction:row;
     justify-content:space-between;
-    padding:0.7rem 1rem;
+    //padding:0.7rem 1rem;
+    padding:0.5rem 0.8rem;
+    font-size: calc(0.7rem + 6 * ((100vw - 320px) / 680));
+    color:#755588;
     border-radius:25px;
-    border: 1px solid violet;
+    border: 1px solid #755588;
     align-items:center;
     z-index:11;
     span{
         margin-left:4px;
+    }
+    ${media.tablet}{
+        font-size: calc(0.5rem + 6 * ((100vw - 320px) / 680));
+    }
+    ${media.laptop}{
+        font-size: calc(0.2rem + 6 * ((100vw - 320px) / 680));
     }
 `;
 
