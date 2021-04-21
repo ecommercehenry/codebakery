e3c6e49072327764d9058f6c2902f5861087c554
johana1211 <johanarezabala1211@gmail.com>
2021-04-21 18:38:13 -0300

    funcionalidad de componente detail

--- client/src/Components/screens/admin/ordenes/OrdenDetail.jsx
+++ client/src/Components/screens/admin/ordenes/OrdenDetail.jsx
@@ -1,82 +1,31 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import styled from "styled-components";
-//NECESITA TRAERSE UNA ORDEN POR ID DE ORDEN...
-//Faltaria recibir el id correcto 
+import React  from "react";
+import  GET_All_ORDERS from "../../../../Apollo/queries/getAllOrders";
+import  { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+
 // Y realizar los Detalles de css 
-const OrderDetail = ({ id }) => {
-let { orders } = useSelector((state) => state.ordersReducer);
+const OrderDetail = () => {
+let { id } = useParams();
+let { data } = useQuery(GET_All_ORDERS)
+let result; 
+result = data?.getAllOrders?.orders.filter((element) => element.id == id)
 
   return (
-    <StyledOrder>
-      {orders[0].id === id ? (
-        orders[0].lineal_order.map((order) => (
+    <div>
+      {result[0]?.lineal_order?.map((element) => (
           <span>
-            <span className='order'>Order: {order.id}</span>
-            <h3 className='namee'>Name: {order.name}</h3>
-            <img className='image' src={order.image} alt=''/>Product
-            <h3 className='cant'>Cant: {order.quantity}</h3>
-            <p className='pricee'>Price: {order.price}</p>
+            <span className='order'>Order: {element.id}</span>
+            <h3 className='namee'>Name: {element.name}</h3>
+            Product: <img className='image' src={element.image} alt=''/>
+            <h3 className='cant'>Cant: {element.quantity}</h3>
+            <h3 className='pricee'>Price: {element.price}</h3>
+            <h3 className='subTotal'>Sub Total: {element.price * element.quantity}</h3>
           </span>
-        ))
-      ) : ''
+      ))
       }
-    </StyledOrder>
+    </div>
   );
 };
 
-const StyledOrder = styled.div`
-
-  .image {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 100%;
-    width: 40rem;
-    //background:blue;
-    height: 40%;
-    overflow: hidden;
-    img {
-
-  .namee {
-    width: 40%;
-    height: 100%;
-    //background:red;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-  }
-  .quantityy {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    width: 18%;
-    height: 90%;
-    //background:violet;
-  }
-  .pricee {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    width: 18%;
-    height: 89%;
-    //padding:0.rem 0;
-    //background:green;
-    box-sizing: border-box;
-    .subtotal {
-      //height:80%;
-      padding-top: 0.5rem;
-      font-size: 1.5rem;
-      //background:red;
-      /* display:flex;
-            flex-direction:column;
-            justify-content:center;
-            align-items:center; */
-      //box-sizing:border-box;
-    }
-  }
-`;
 
 export default OrderDetail;
