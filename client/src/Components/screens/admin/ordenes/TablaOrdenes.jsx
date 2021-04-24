a8a2fb5a11b8e5f5ef3e27ca36787aa4b603537e
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-24 13:22:19 -0500

    Delete filters not functional

--- client/src/Components/screens/admin/ordenes/TablaOrdenes.jsx
+++ client/src/Components/screens/admin/ordenes/TablaOrdenes.jsx
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { saveOrders } from "../../../../actions";
 import { toast } from "react-toastify";
 import ButtonClear from "./ButtonClear";
-import CheckFilters from "./CheckFilters";
+// import CheckFilters from "./CheckFilters";
 
 //traerme todas las ordenes hechas.. estan en la BD--Uso query de Apollo
 //guardo en el reducer ordersReducer las ordenes para aplicar busquedas y filtros uso dispatch
@@ -56,7 +56,7 @@ export default function TablaOrdenes() {
   return (
     <StyledTablaOrdenes>
       <ButtonClear name="Clear" />
-      <CheckFilters />
+      {/* <CheckFilters /> */}
       {/* Checkbox filters */}
       { dataRENDER ? (
         dataRENDER.map((ord) => {
