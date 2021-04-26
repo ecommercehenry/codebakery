dcf464f925011215ec85af666fcb40f9561bce0d
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-04-26 18:56:46 -0500

    Agrega funcionalidad de buscar dinámicamente  y agrega funcionalidad para order por nombre

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,6 +14,10 @@ export const CHANGE_PAGE = "CHANGE_PAGE";
 export const CHECKBOX_CHANGE = "CHECKBOX_CHANGE";
 export const CLEAR_CHECKBOXES = "CLEAR_CHECKBOXES";
 export const FILTER_NAME = "FILTER_NAME";
+export const GET_ALL_USERS_WITH_DETAILS = "GET_ALL_USERS_WITH_DETAILS"
+export const ORDER_ASC_BY_NAME = "ORDER_ASC_BY_NAME"
+export const ORDER_DESC_BY_NAME = "ORDER_DESC_BY_NAME"
+export const SEARCH_BY_NAME = "SEARCH_BY_NAME"
 
 export const setSearch = () => {
   return {
@@ -118,3 +122,31 @@ export const checkboxChange = (id) => {
 export const clearCheckboxes = () => {
   return { type : CLEAR_CHECKBOXES }
 }
+
+export const getAllUsersWithDetails = (data) => {
+  return { 
+    type : GET_ALL_USERS_WITH_DETAILS,
+    payload: data 
+  }
+}
+
+export const orderAscByName = () => {
+  return { 
+    type : ORDER_ASC_BY_NAME,
+  }
+}
+
+export const orderDescByName = () => {
+  return { 
+    type : ORDER_DESC_BY_NAME,
+  }
+}
+
+export const searchByName = (payload) => {
+  return { 
+    type : SEARCH_BY_NAME,
+    payload,
+  }
+}
+
+
---
+++ client/src/reducer/userAdmin.js
@@ -0,0 +1,69 @@
+import { GET_ALL_USERS_WITH_DETAILS } from "../actions/index";
+import { ORDER_ASC_BY_NAME,  ORDER_DESC_BY_NAME, SEARCH_BY_NAME } from "../actions/index"
+
+const initialState = {
+  dataUserAdmin: [],
+  dataToRender: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_USERS_WITH_DETAILS: 
+        // console.log(action.payload) 
+      if (Array.isArray(action.payload)) {
+        var data = [...action.payload]
+        // console.log(data)
+        return {
+            ...state,
+            dataUserAdmin: data.sort((a, b) => {
+                return b.id - a.id
+              }),
+            dataToRender: data.sort((a, b) => {
+                return a.id - b.id
+              }),
+      } 
+    } else {
+          return {
+              ...state,
+              dataUserAdmin: action.payload,
+              dataToRender: action.payload
+          }
+      }
+    case ORDER_ASC_BY_NAME:
+        // var data = state.dataUserAdmin.sort((a, b) => {
+        //     return  a.name - b.name})
+        // console.log("data reducer", data)
+        return {
+            ...state,
+            dataToRender: state.dataUserAdmin.sort(function(a, b) {
+                return a.name.localeCompare(b.name)
+        })
+    }
+    case ORDER_DESC_BY_NAME:
+        // var data = state.dataUserAdmin.sort((a, b) => {
+        //     return  a.name - b.name})
+        // console.log("data reducer", data)
+        return {
+            ...state,
+            dataToRender: state.dataUserAdmin.sort(function(a, b) {
+                return b.name.localeCompare(a.name)
+        })
+    }
+    case SEARCH_BY_NAME:
+        if(action.payload === "all") {
+            return {
+                ...state,
+                dataToRender: state.dataUserAdmin
+            }
+        } else {
+            return {
+                ...state,
+                dataToRender: state.dataUserAdmin.filter(obj => obj.name.includes(action.payload))
+            }
+        }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
