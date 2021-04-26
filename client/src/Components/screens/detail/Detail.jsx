3294e77508aed708d24b401ef847038fb1a142f3
lucasacostaa <lucasuracosta@gmail.com>
2021-04-26 12:16:36 -0300

    Fix detail nest route

--- client/src/Components/screens/detail/Detail.jsx
+++ client/src/Components/screens/detail/Detail.jsx
@@ -1,35 +1,33 @@
 import React, { useEffect } from "react";
 import "./DetailStyles.css";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import getData from "../../../Apollo/queries/productById";
 import { useQuery } from "@apollo/client";
 import ButtonAddCart from "../../screens/catalogue/products/grid/ButtonAddCart";
-import ProductReview from "../reviews/ProductReview"
-import styled from 'styled-components'
+import ProductReview from "../reviews/ProductReview";
+import styled from "styled-components";
+import { FlexboxGrid } from "rsuite";
 
 const Detail = () => {
   let idCart = useParams();
-  useEffect(() => {
-    window.scrollTo(0, 0);
+  /* useEffect(() => {
 
-    setTimeout(() => {
-      document.body.style.overflow = "hidden";
-    }, 1000);
+    document.body.style.overflow = "visible"
 
     return () => {
       document.body.style.overflow = "visible";
     };
-  }, []);
+  }, []); */
 
   const path = window.location.pathname;
   const id = parseInt(path.split("/").pop(), 10);
-  let {status} = useSelector((state)=>state.theme);
+  let { status } = useSelector((state) => state.theme);
   const { data } = useQuery(getData, { variables: { id } }); // <------
   useEffect(() => {}, [data]);
 
   return (
-    <div className="detail-container" >
+    <StyledDetailModal className="detail-container">
       <Link to="/catalogue" className="close-btn">
         X
       </Link>
@@ -46,8 +44,10 @@ const Detail = () => {
             </div>
             <div className="dataSide d-flex position-relative">
               <>
+              <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
                 <span className="title">{data?.productById.name}</span>
                 <span className="price">${data?.productById.price}</span>
+              </div>
                 <span className="description">
                   {data?.productById.description}
                 </span>
@@ -65,105 +65,21 @@ const Detail = () => {
           "loading..."
         )}
       </StyledDetail>
-    </div>
+    </StyledDetailModal>
   );
 };
 
-const StyledDetail = styled.div`
-  background:${({light})=>light 
-  ? 'white' 
-  : '#222222'};
-  color:${({light})=>light 
-  ? 'inherit' 
-  : 'white'};
-
-.detail-container {
-    position: absolute;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: horizontal;
-    -webkit-box-direction: normal;
-        -ms-flex-direction: row;
-            flex-direction: row;
-    -webkit-box-align: start;
-        -ms-flex-align: start;
-            align-items: flex-start;
-    -webkit-box-pack: center;
-        -ms-flex-pack: center;
-            justify-content: center;
-    height: 100vh;
-    width: 100vw;
-    background-color: rgba(0, 0, 0, 0.76);
-    z-index: 2;
-    margin: 0 auto;
-    right: auto;
-    top: 0px;
-    border: solid grey;
-  }
-  
-  .detail-container .detailCard {
-    position: relative;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: horizontal;
-    -webkit-box-direction: normal;
-        -ms-flex-direction: row;
-            flex-direction: row;
-    -webkit-box-pack: center;
-        -ms-flex-pack: center;
-            justify-content: center;
-    -webkit-box-align: center;
-        -ms-flex-align: center;
-            align-items: center;
-    width: 90%;
-    height: 80%;
-    /* background-color: white; */
-    margin: auto;
-    border-radius: 40px;
-  }
-  
-  .detail-container .detailCard .imageSide {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-        -ms-flex-direction: column;
-            flex-direction: column;
-    -webkit-box-align: center;
-        -ms-flex-align: center;
-            align-items: center;
-    -webkit-box-pack: center;
-        -ms-flex-pack: center;
-            justify-content: center;
-    height: 100%;
-    width: 50%;
-  }
-  
-  .detail-container .detailCard .dataSide {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-        -ms-flex-direction: column;
-            flex-direction: column;
-    position: relative;
-    -webkit-box-align: center;
-        -ms-flex-align: center;
-            align-items: center;
-    -webkit-box-pack: flex-start;
-        -ms-flex-pack: flex-start;
-            justify-content: flex-start;
-    height: 100%;
-    width: 50%;
-    padding: 5%;
-    padding-right: 10%;
-  }
-  
-  .detail-container .close-btn {
+const StyledDetailModal = styled.div`
+    position: fixed;
+    background-color: rgba(0, 0, 0, 0.445);
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 999;
+    transition: all 0.3s;
+     
+    .close-btn {
     position: absolute;
     top: 2%;
     right: 2%;
@@ -174,73 +90,111 @@ const StyledDetail = styled.div`
     transition: 300ms ease;
     z-index: 4;
   }
+`
+const StyledDetail = styled.div`
+  background: ${({ light }) => (light ? "white" : "#222222")};
+  color: ${({ light }) => (light ? "inherit" : "white")};
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 80vw;
+    height: 80vh;
+    background-color: white; 
+    margin: auto;
+    border-radius: 40px;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    
+    .imageSide {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 50%;
+    }
 
-  .title{
-      font-size: 1.5em;
-      letter-spacing: 0;
-      text-align: center;
+    .dataSide {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+    width: 50%;
+    padding: 5%;
+    padding-right: 10%;
   }
 
-  .price{
-      font-size: 1.3rem;
-      color: #755588;
-      font-weight: bold;
+  .title {
+    font-size: 1.5em;
+    letter-spacing: 0;
+    text-align: center;
   }
 
-  .description{
-      font-size: 1.0rem;
-      margin: 10% 0;
+  .price {
+    margin-top: 1rem;
+    font-size: 1.3rem;
+    color: #755588;
+    font-weight: bold;
   }
 
-.up-btns{
-    width: 80%
-}
+  .description {
+    font-size: 1rem;
+    margin: 10% 0;
+  }
 
-.up-btns input {
+  .up-btns {
+    width: 80%;
+  }
+
+  .up-btns input {
     width: 40%;
-    justify-content: space-evenly!important
-}
+    justify-content: space-evenly !important;
+  }
 
-.bottom-btns{
+  .bottom-btns {
     margin-top: 4%;
     width: 100%;
     display: flex;
     justify-content: space-evenly !important;
-}
+  }
 
-.all-btn{
+  .all-btn {
     bottom: 15%;
-    width: 80%
-}
+    width: 80%;
+  }
 
-
-.grey-btn{
+  .grey-btn {
     margin-left: 14px;
     display: block;
-    height:  30px;
-    width: fit-content!important;
+    height: 30px;
+    width: fit-content !important;
     border-radius: 20px;
     border: none;
-    background-color: #CECECE;
+    background-color: #cecece;
     text-decoration: none !important;
     color: black;
     font-weight: bold;
     font-size: 0.9rem;
-    padding: 0 5% 0.5% 5%; 
-}
+    padding: 0 5% 0.5% 5%;
+  }
 
-.cart-btn{
+  .cart-btn {
     background-size: 20px 20px;
-}
+  }
 
-.select-quantity{
+  .select-quantity {
     border-radius: 40px;
     border: solid 1px;
-    border-color: #CECECE;
+    border-color: #cecece;
     padding: 2%;
-}
+  }
 
-.purple-btn {
+  .purple-btn {
     display: flex;
     width: fit-content !important;
     border-radius: 40px;
