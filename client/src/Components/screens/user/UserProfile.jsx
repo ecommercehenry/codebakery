f25345096a81b3c834b5c9b5d39a8af1cce486cb
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-05-03 18:50:16 -0500

    Ajustes onChange inputs y reset password user

--- client/src/Components/screens/user/UserProfile.jsx
+++ client/src/Components/screens/user/UserProfile.jsx
@@ -6,16 +6,17 @@ import { useParams } from "react-router";
 import MODIFY_USER from "../../../Apollo/mutations/modifyUser";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
-import closeIcon from "../../../icons/close2.svg"; 
+import closeIcon from "../../../icons/close2.svg";
 //import SettingsIcon from "@material-ui/icons/Settings";
 
 const UserProfile = () => {
   let { id } = useParams();
 
-  const [click, setClick] = useState(1)
+  const [click, setClick] = useState(1);
   const [modifyUser, { data }] = useMutation(MODIFY_USER, {
     variables: { id: parseInt(id) },
     fetchPolicy: "no-cache",
+    refetchQueries: [{ query: getUserById }],
   });
   const { data: $USER } = useQuery(getUserById, {
     variables: { id: parseInt(id) },
@@ -23,39 +24,39 @@ const UserProfile = () => {
   });
 
   const [input, setInput] = useState({
-    name: '', 
-    address: '', 
-    email: '', 
-    dni: '', 
-    password: '', 
-    phoneNumber: '', 
+    name: "",
+    address: "",
+    email: "",
+    dni: "",
+    password: "",
+    phoneNumber: "",
+  });
 
-})
-const inputHandler =  (e) => {
-  setInput({
-      ...input, 
-  [e.target.value]:  e.target.value
-  })
-  
-}
-  const submitHandler = async (e) => {
-    e.preventDefault(); 
-    await modifyUser({
-        variables: {
-            name: input.name, 
-            email: input.email, 
-            address: input.address, 
-            dni: input.dni, 
-            phoneNumber: input.phoneNumber, 
-            password: input.password
+  const inputHandler = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-        }
-    }) 
-}
+  const submitHandler = (e) => {
+    // e.preventDefault();
+    modifyUser({
+      variables: {
+        name: input.name,
+        email: input.email,
+        address: input.address,
+        dni: input.dni,
+        phoneNumber: input.phoneNumber,
+        newPassword: input.password,
+      },
+    });
+  };
 
-  return (
-    click === 1 ? (
-      <StyledUseer>
+  return click === 1 ? (
+    <StyledUseer>
       {
         <div key={id} className="container">
           <h1>Profile</h1>
@@ -64,8 +65,8 @@ const inputHandler =  (e) => {
               <span>Name</span>
               <p>{$USER?.getUserById.name}</p>
               <Button value="name" onClick={() => setClick(2)}>
-          Editar
-        </Button>
+                Editar
+              </Button>
             </div>
           </div>
           <div className="element-naame">
@@ -73,8 +74,8 @@ const inputHandler =  (e) => {
               <span>Document</span>
               <p>{$USER?.getUserById.dni}</p>
               <Button value="document" onClick={() => setClick(3)}>
-          Editar
-        </Button>
+                Editar
+              </Button>
             </div>
           </div>
           <div className="element-naame">
@@ -82,8 +83,8 @@ const inputHandler =  (e) => {
               <span>Email</span>
               <p>{$USER?.getUserById.email}</p>
               <Button value="email" onClick={() => setClick(4)}>
-          Editar
-        </Button>
+                Editar
+              </Button>
             </div>
           </div>
           <div className="element-naame">
@@ -91,8 +92,8 @@ const inputHandler =  (e) => {
               <span>PhoneNumber</span>
               <p>{$USER?.getUserById.phoneNumber}</p>
               <Button value="phoneNumber" onClick={() => setClick(5)}>
-          Editar
-        </Button>
+                Editar
+              </Button>
             </div>
           </div>
           <div className="element-naame">
@@ -100,25 +101,24 @@ const inputHandler =  (e) => {
               <span>Address</span>
               <p>{$USER?.getUserById.address}</p>
               <Button value="address" onClick={() => setClick(6)}>
-          Editar
-        </Button>
+                Editar
+              </Button>
             </div>
           </div>
           <div className="element-naame">
             <div className="text-container">
               <span>Password</span>
-              <p>{$USER?.getUserById.address}</p>
-              <Button value="address" onClick={() => setClick(7)}>
-          Editar
-        </Button>
+              <p>{$USER?.getUserById.password}</p>
+              <Button value="password" onClick={() => setClick(7)}>
+                Editar
+              </Button>
             </div>
           </div>
         </div>
       }
     </StyledUseer>
-
-    ) : click === 2 ? (
-      <div
+  ) : click === 2 ? (
+    <div
       style={{
         display: "flex",
         justifyContent: "center",
@@ -133,9 +133,10 @@ const inputHandler =  (e) => {
         paddingLeft: "10vw",
       }}
     >
-
-      <StyledForm  onSubmit={submitHandler}>
-    <button onClick={()  => setClick(1)} className="closeee"><img src={closeIcon} width="30px" display="flex" alt="closeIcon"/></button> 
+      <StyledForm onSubmit={submitHandler}>
+        <button onClick={() => setClick(1)} className="closeee">
+          <img src={closeIcon} width="30px" display="flex" alt="closeIcon" />
+        </button>
         <div className="infoProductt">
           <div className="namee">
             <label>Name</label>
@@ -144,20 +145,17 @@ const inputHandler =  (e) => {
               type="text"
               placeholder="Name"
               value={input.name}
-              onChange={inputHandler}
+              onChange={(e) => inputHandler(e)}
             />
           </div>
           <div className="submitt">
             <button type="submit">Update</button>
           </div>
-      </div>
+        </div>
       </StyledForm>
-
-      </div>
- 
-    ) : click === 3 ? (
-
-      <div
+    </div>
+  ) : click === 3 ? (
+    <div
       style={{
         display: "flex",
         justifyContent: "center",
@@ -172,8 +170,10 @@ const inputHandler =  (e) => {
         paddingLeft: "10vw",
       }}
     >
-      <StyledForm  onSubmit={submitHandler}>
-      <button onClick={()  => setClick(1)} className="closeee"><img src={closeIcon} width="30px" display="flex" alt="closeIcon"/></button> 
+      <StyledForm onSubmit={submitHandler}>
+        <button onClick={() => setClick(1)} className="closeee">
+          <img src={closeIcon} width="30px" display="flex" alt="closeIcon" />
+        </button>
         <div className="infoProductt">
           <div className="namee">
             <label>Document</label>
@@ -182,19 +182,17 @@ const inputHandler =  (e) => {
               type="text"
               placeholder="Document"
               value={input.dni}
-              onChange={inputHandler}
+              onChange={(e) => inputHandler(e)}
             />
           </div>
           <div className="submitt">
             <button type="submit">Update </button>
           </div>
-      </div>
+        </div>
       </StyledForm>
-
-      </div>
-    ) :  click === 4 ? (
-
-      <div
+    </div>
+  ) : click === 4 ? (
+    <div
       style={{
         display: "flex",
         justifyContent: "center",
@@ -209,8 +207,10 @@ const inputHandler =  (e) => {
         paddingLeft: "10vw",
       }}
     >
-      <StyledForm  onSubmit={submitHandler}>
-     <button onClick={()  => setClick(1)} className="closeee"><img src={closeIcon} width="30px" display="flex" alt="closeIcon"/></button> 
+      <StyledForm onSubmit={submitHandler}>
+        <button onClick={() => setClick(1)} className="closeee">
+          <img src={closeIcon} width="30px" display="flex" alt="closeIcon" />
+        </button>
         <div className="infoProductt">
           <div className="namee">
             <label>Email</label>
@@ -219,19 +219,17 @@ const inputHandler =  (e) => {
               type="text"
               placeholder="Email"
               value={input.email}
-              onChange={inputHandler}
+              onChange={(e) => inputHandler(e)}
             />
           </div>
           <div className="submitt">
             <button type="submit">Update</button>
           </div>
-      </div>
+        </div>
       </StyledForm>
-
-      </div>
-    ) : click === 5 ? (
-
-      <div
+    </div>
+  ) : click === 5 ? (
+    <div
       style={{
         display: "flex",
         justifyContent: "center",
@@ -246,9 +244,10 @@ const inputHandler =  (e) => {
         paddingLeft: "10vw",
       }}
     >
-        
-      <StyledForm  onSubmit={submitHandler}>
-    <button onClick={()  => setClick(1)} className="closeee"><img src={closeIcon} width="30px" display="flex" alt="closeIcon"/></button> 
+      <StyledForm onSubmit={submitHandler}>
+        <button onClick={() => setClick(1)} className="closeee">
+          <img src={closeIcon} width="30px" display="flex" alt="closeIcon" />
+        </button>
         <div className="infoProductt">
           <div className="namee">
             <label>Phone Number</label>
@@ -257,19 +256,17 @@ const inputHandler =  (e) => {
               type="text"
               placeholder="Phone Number"
               value={input.phoneNumber}
-              onChange={inputHandler}
+              onChange={(e) => inputHandler(e)}
             />
           </div>
           <div className="submitt">
             <button type="submit">Update</button>
           </div>
-      </div>
+        </div>
       </StyledForm>
-
-      </div>
-    ) : click === 6 ? (
-
-      <div
+    </div>
+  ) : click === 6 ? (
+    <div
       style={{
         display: "flex",
         justifyContent: "center",
@@ -284,28 +281,66 @@ const inputHandler =  (e) => {
         paddingLeft: "10vw",
       }}
     >
-      <StyledForm  onSubmit={submitHandler}>
-      <button onClick={()  => setClick(1)} className="closeee"><img src={closeIcon} width="30px" display="flex" alt="closeIcon"/></button> 
+      <StyledForm onSubmit={submitHandler}>
+        <button onClick={() => setClick(1)} className="closeee">
+          <img src={closeIcon} width="30px" display="flex" alt="closeIcon" />
+        </button>
         <div className="infoProductt">
           <div className="namee">
-            <label>Password</label>
+            <label>Address</label>
             <input
-              name="password"
+              name="address"
               type="text"
-              placeholder="Password"
-              value={input.Password}
-              onChange={inputHandler}
+              placeholder="Address"
+              value={input.address}
+              onChange={(e) => inputHandler(e)}
             />
           </div>
           <div className="submitt">
             <button type="submit">Update</button>
           </div>
-      </div>
+        </div>
       </StyledForm>
-      </div>
-
-    ) : <p>''</p>
-
+    </div>
+  ) : click === 7 ? (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.664)",
+        zIndex: 5,
+        position: "fixed",
+        height: "100vh",
+        width: "100vw",
+        top: "0",
+        left: "0",
+        paddingLeft: "10vw",
+      }}
+    >
+      <StyledForm onSubmit={submitHandler}>
+        <button onClick={() => setClick(1)} className="closeee">
+          <img src={closeIcon} width="30px" display="flex" alt="closeIcon" />
+        </button>
+        <div className="infoProductt">
+          <div className="namee">
+            <label>Password</label>
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={input.password}
+              onChange={(e) => inputHandler(e)}
+            />
+          </div>
+          <div className="submitt">
+            <button type="submit">Update</button>
+          </div>
+        </div>
+      </StyledForm>
+    </div>
+  ) : (
+    <p>''</p>
   );
 };
 
@@ -339,89 +374,88 @@ const StyledUseer = styled.div`
 `;
 
 const StyledForm = styled.form`
-width:35%;
-height: 80vh;
-background: white;
-border-radius:65px;
-padding: 3rem 4rem;
-border:1px solid #f3dff3;
-position: relative;
+  width: 35%;
+  height: 80vh;
+  background: white;
+  border-radius: 65px;
+  padding: 3rem 4rem;
+  border: 1px solid #f3dff3;
+  position: relative;
 
-.closeee{
-  display: flex;
-  justify-content: flex-end;
+  .closeee {
+    display: flex;
+    justify-content: flex-end;
+  }
 
-}
-
-.infoProductt{
+  .infoProductt {
     //background:blue;
-    display:flex;
-    flex-direction:column;
-    justify-content:space-between;
-    .namee{
-        //background:green;
-        height:20%;
-        display:flex;
-        flex-direction:column;
-        justify-content:center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .namee {
+      //background:green;
+      height: 20%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
     }
-    .descriptionn{
-        //background:green;
-        height:20%;
-        display:flex;
-        flex-direction:column;
-        justify-content:center;
-        textarea{
-          width:100%;
-          margin-top:0.5rem;
-          padding: 0 1rem;
-          //height:3rem;
-          border-radius:23px;
-          border:1px solid #dad7dc;
-          background:#E3DDE7;
-      }
-    }
-    input{
-        width:100%;
-        margin-top:0.8rem;
+    .descriptionn {
+      //background:green;
+      height: 20%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      textarea {
+        width: 100%;
+        margin-top: 0.5rem;
         padding: 0 1rem;
-        height:3rem;
-        border-radius:23px;
-        border:1px solid #dad7dc;
-        background:#E3DDE7;
+        //height:3rem;
+        border-radius: 23px;
+        border: 1px solid #dad7dc;
+        background: #e3dde7;
       }
-    .ratiing{
+    }
+    input {
+      width: 100%;
+      margin-top: 0.8rem;
+      padding: 0 1rem;
+      height: 3rem;
+      border-radius: 23px;
+      border: 1px solid #dad7dc;
+      background: #e3dde7;
+    }
+    .ratiing {
       padding: 3rem 4rem;
-      display:flex;
+      display: flex;
       justify-content: center;
       align-items: center;
     }
-    .ratingg{
+    .ratingg {
       padding: 3rem 4rem;
-      display:flex;
+      display: flex;
       justify-content: center;
       align-items: center;
     }
-    .submitt{
-        height:10%;
-        display:flex;
-        justify-content: center;
+    .submitt {
+      height: 10%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      //background:red;
+      button {
+        background: #5e3f71;
+        color: white;
+        width: 23%;
+        height: 3rem;
+        font-size: 1rem;
+        display: flex;
         align-items: center;
-        //background:red;
-        button{
-            background:#5E3F71;
-            color:white;
-            width: 23%;
-            height:3rem;
-            font-size:1.0rem;
-            display:flex;
-            align-items: center;
-            justify-content:center;
-            border-radius:20px;
-            padding: 0 2.3rem;
-            border:none;
-            cursor:pointer;
-        }
+        justify-content: center;
+        border-radius: 20px;
+        padding: 0 2.3rem;
+        border: none;
+        cursor: pointer;
+      }
     }
-
+  }
 `;
