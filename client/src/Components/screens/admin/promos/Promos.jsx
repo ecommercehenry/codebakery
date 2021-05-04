609a94592b6e2d9180874b4ed8f224d998706d4f
Santiago Rincon <santiagorincon2001@gmail.com>
2021-05-04 11:36:11 -0500

    No used imports

--- client/src/Components/screens/admin/promos/Promos.jsx
+++ client/src/Components/screens/admin/promos/Promos.jsx
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react'
+import React, {useEffect} from 'react'
 import FormPromo from "./FormPromo"
 import PromoList from "./PromoList"
 import styled from 'styled-components'
