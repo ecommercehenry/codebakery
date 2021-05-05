2d3fd008e50f607090b7c55935c0414015c94b22
Pablo Ramiro Garay <68715166+814942@users.noreply.github.com>
2021-05-04 21:10:55 -0300

    Update serviceTesting.js
    Error de duplicidad corregido

--- api/serviceTesting.js
+++ api/serviceTesting.js
@@ -1,28 +1,13 @@
 /*****CUAL ES LA UTILIDAD DE ESTE ARCHIVO?
  * PROBAR SERVICIOS DIRECTAMENTE SIN TENER QUE PASAR POR GRAPHQL
  */
-
 const { conn } = require("./src/db.js");
 const { dataPopulation } = require("./src/sequelize/dataPopulation");
-const {
-  sendEmail,
-  getFormatedMessage,
-} = require("./src/services/emailService.js");
-const {
-  getAllOrders,
-  getOrderById,
-  updateOrderToTicket,
-} = require("./src/services/orderService");
-const { addReview, modifyReview } = require("./src/services/reviewsService.js");
-const { saveImageSlider } = require("./src/services/imagesService");
-
- const { conn } = require("./src/db.js");
- const { dataPopulation } = require("./src/sequelize/dataPopulation");
 const { sendEmail, getFormatedMessage } = require("./src/services/emailService.js");
- const {getAllOrders,getOrderById, updateOrderToTicket} = require("./src/services/orderService");
+const {getAllOrders,getOrderById, updateOrderToTicket} = require("./src/services/orderService");
 const { addReview , modifyReview} = require("./src/services/reviewsService.js");
- const {sendNewsletter} = require ("./src/services/newsletterService")
- const {getTokenResetPassword} = require("./src/services/userService")
+const {sendNewsletter} = require ("./src/services/newsletterService")
+const {getTokenResetPassword} = require("./src/services/userService")
   
      
  // Syncing all the models at once.
