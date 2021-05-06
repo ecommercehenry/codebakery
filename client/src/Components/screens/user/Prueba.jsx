fe0b4b72cb1671f38191b115e88dccedb4bfa868
johana1211 <johanarezabala1211@gmail.com>
2021-05-06 14:07:22 -0300

    tablaDeOrdensCss

---
+++ client/src/Components/screens/user/Prueba.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import styled from "styled-components";
+import GET_ALL_ORDERS_USER from "../../../Apollo/queries/getAllOrdersUser";
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 375,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 1px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  boton: {
+    background: '#5e3f71', 
+  },
+});
+
+const Prueba = ({ setClick, index }) => {
+
+  let { id } = useParams();
+  const { data } = useQuery(GET_ALL_ORDERS_USER, {
+    variables: {
+      userId: parseInt(id),
+      fetchPolicy: "no-cache",
+    },
+  });
+  let result; 
+  if(data){
+    if(data.getAllOrdersUser){
+    result =  data.getAllOrdersUser.orders.filter((e) => e.id === index)
+        
+    }
+  }
+
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <StyledDetail >
+     
+
+            {result[0].lineal_order?.map(
+              (sectionId) => (
+               
+                <Card className={classes.root} variant="outlined">
+                <CardContent>
+                  <Typography className={classes.pos} color="textSecondary" gutterBottom>
+                    Product 
+                    <br/> {sectionId.name}
+                  </Typography>
+                  <Typography className={classes.title} color="textSecondary">
+                    Price
+                    <br/> {sectionId.price}
+                  </Typography>
+                  <Typography className={classes.title} color="textSecondary">
+                    Quantity
+                    <br/> {sectionId.quantity}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    Total Price
+                    <br/> $ {sectionId.quantity* sectionId.quantity}
+                    <br />
+                    {'"Code Backery"'}
+                  </Typography>
+                  <CardActions>
+        <Button className={classes.boton}onClick={() => setClick(1)} size="small">Exit</Button>
+      </CardActions>
+                </CardContent>
+              </Card>
+
+              )
+            )}
+
+     
+    </StyledDetail>
+  );
+};
+
+export default Prueba;
+const StyledDetail = styled.div`
+overflow: scroll;
+    white-space: normal;
+    width: 60%;
+    overflow-x: overlay;
+    position: absolute;
+    left: 18rem;
+    top: 4rem;
+
+`;
--- client/src/Components/screens/user/UserProduct.jsx
+++ client/src/Components/screens/user/UserProduct.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState  } from "react";
 import GET_ALL_ORDERS_USER from "../../../Apollo/queries/getAllOrdersUser";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
@@ -13,7 +13,10 @@ import InboxIcon from "@material-ui/icons/MoveToInbox";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import StarBorder from "@material-ui/icons/StarBorder";
-
+import Button from "@material-ui/core/Button";
+import styled from "styled-components";
+import closeIcon from "../../../icons/close2.svg"; 
+import Prueba from "./Prueba"; 
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,17 +32,25 @@ const useStyles = makeStyles((theme) => ({
     paddingLeft: theme.spacing(4),
   },
   titlee: {
-
-    position: 'absolute',
-    left: '15rem',
-    top: '1.5rem',
-  }, 
+    position: "absolute",
+    left: "15rem",
+    top: "1.5rem",
+  },
 }));
 
 export default function UserProduct() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [click, setClick] = useState(1); 
 
+  console.log(click, 'click')
+  const classes = useStyles();
+  const [index, setIndex ]= useState(null)
+  const [open, setOpen] = useState(false);
+
+  const detailHandler = (id) => {
+    setClick(2)
+    setIndex(id)
+
+  }
   const handleClick = () => {
     setOpen(!open);
   };
@@ -52,14 +63,14 @@ export default function UserProduct() {
   });
 
   return (
-    <div>
+    click === 1 ? 
+    (<div>
       <h1 className={classes.titlee}>Orders History</h1>
       <List className={classes.root} subheader={<li />}>
         {data?.getAllOrdersUser?.orders.map((sectionId) => (
           <li key={`section-${sectionId.id}`} className={classes.listSection}>
             <ul className={classes.ul}>
               <ListSubheader>{`Orders ${sectionId.id}`}</ListSubheader>
-              <ListSubheader>{`Status ${sectionId.status}`}</ListSubheader>
               <ListItem button onClick={handleClick}>
                 <ListItemIcon>
                   <InboxIcon />
@@ -70,10 +81,15 @@ export default function UserProduct() {
                 <Collapse in={open} timeout="auto" unmountOnExit>
                   <List component="div" disablePadding>
                     <ListItem button className={classes.nested}>
-                      <ListItemIcon>
-                        <StarBorder />
-                      </ListItemIcon>
-                      {
+                      <ListItem key={`item-${sectionId.id}`}>
+                        <ListItemText
+                          primary={`Status ${sectionId.status} `}
+                        />
+                        <Button value="name" onClick={() => detailHandler(sectionId.id)} >
+                          detail
+                        </Button>
+                      </ListItem>
+                      {/* {
                      sectionId.lineal_order.map(
                         (item) => (
                             <ListItem key={`item-${sectionId.id}-${item}`}>
@@ -81,7 +97,7 @@ export default function UserProduct() {
                             <ListItemText primary={`Price $ ${item.price}`} />
                           </ListItem>
                         )
-                      )}
+                      )} */}
                     </ListItem>
                   </List>
                 </Collapse>
@@ -91,8 +107,65 @@ export default function UserProduct() {
         ))}
       </List>
       ;
-    </div>
+    </div>) : (
+      <Prueba setClick={setClick} click={click} setIndex={setIndex} index={index}/> 
+  //  <div style={{
+  //       display: "flex",
+  //       justifyContent: "center",
+  //       alignItems: "center",
+  //       backgroundColor: "rgba(0, 0, 0, 0.664)",
+  //       zIndex: 5,
+  //       position: "fixed",
+  //       height: "100vh",
+  //       width: "100vw",
+  //       top: "0",
+  //       left: "0",
+  //       paddingLeft: "10vw",
+  //     }}
+  //   >
+  //     <StyledForm>
+  //     <button onClick={()  => setClick(1)} className="closeee"><img src={closeIcon} width="30px" display="flex" alt="closeIcon"/></button> 
+  //       <div className="infoProductt">
+  //         <div className="namee">
+            
+  //           <label>Password</label>
+  //           {
+  //           (console.log(click, 'seguncdo'))
+  //           }
+
+  //         </div>
+  //     </div>
+  //     </StyledForm>
+  // </div>
+
+    ) 
   );
 }
 
+const StyledForm = styled.form`
+width:35%;
+height: 80vh;
+background: white;
+border-radius:65px;
+padding: 3rem 4rem;
+border:1px solid #f3dff3;
+position: relative;
 
+.closeee{
+  display: flex;
+  justify-content: flex-end;
+
+}
+.infoProductt{
+  //background:blue;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-between;
+  .namee{
+      //background:green;
+      height:20%;
+      display:flex;
+      flex-direction:column;
+      justify-content:center;
+  }
+`; 
