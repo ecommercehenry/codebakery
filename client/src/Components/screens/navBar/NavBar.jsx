27d01580c29a6a1e2bf90aa0ce741abfbb95d844
franciscotov <54585008+franciscotov@users.noreply.github.com>
2021-05-13 13:58:54 -0300

    Conflictos varios (#6)
    * se resuelven algunos conflictos
    
    * resuelvo conflictos de redireccion, mÃas cambios anteriores
    
    * Fixed redirect
    
    Co-authored-by: Santiago Rincon <santiagorincon2001@gmail.com>

--- client/src/Components/screens/navBar/NavBar.jsx
+++ client/src/Components/screens/navBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { useMutation, useQuery } from "@apollo/client";
 import APPLY_DISCOUNT from "../../../Apollo/mutations/applyDiscount";
 import RESET_DISCOUNT from "../../../Apollo/mutations/resetDiscount";
@@ -39,7 +39,8 @@ const NavBar = ({ color }) => {
 
   let logout = () => {
     localStorage.clear();
-    window.location.reload();
+    window.location.href = "/";
+    // window.location.reload();
   };
 
   let storage = window.localStorage;
